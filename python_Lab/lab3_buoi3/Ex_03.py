14852e960c6c234ea45e6a5803744e8f63d6a3db
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-04-08 01:50:39 +0700

    okeeee

--- python_Lab/lab3_buoi3/Ex_03.py
+++ python_Lab/lab3_buoi3/Ex_03.py
@@ -1,1 +1,89 @@
-# okeoke
+#import random
+
+"""
+1. Write a program that prints numbers from 1 to 10
+"""
+def ex_01():
+    for i in range(1, 11):
+        print(i)
+
+
+
+"""
+2. Write a program to calculate the sum of numbers in a range from 1 to n (n is entered from the keyboard)
+"""
+def ex_02():
+    n = int(input("Enter the range: "))
+    sum = 0
+    for i in range(1, n+1):
+        sum = (n * (n+1))/2
+    print(f"The sum of numbers in a range from 1 to {n}: {sum}")
+
+
+
+"""
+3. Write a program to calculate the sum of even (/odd) numbers in a range from 1 to n (n is entered from the keyboard)
+"""
+def ex_03():
+    n = int(input("Enter the range: "))
+    sum_even = 0
+    sum_odd = 0
+    for i in range(1, n+1):
+        if i%2==0:
+            sum_even += i
+        else:
+            sum_odd += i
+
+    print(f"The sum of even numbers in a range from 1 to {n}: {sum_even}")
+    print(f"The sum of odd numbers in a range from 1 to {n}: {sum_odd}")
+
+
+
+"""
+4. Write a program to check how many vowels are in a string entered from the keyboard.
+"""
+def ex_04():
+    string = input("Enter the string: ")
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in string.lower():
+        if char in vowels:
+            count += 1
+    print(f"There are {count} vowels in the string.")
+
+
+
+"""
+5. Write a program to count the number of words in a sentence the user enters.
+"""
+def ex_05():
+    sentence = input("Enter the sentence: ")
+    count = 1
+    for word in sentence:
+        if word == ' ':
+            count += 1
+    print(f"There are {count} words in the sentence: {sentence}")
+
+
+
+"""
+6. Write a program that implements a game as the following description:
+
+	1. The computer generates a random number from 1 to 100
+
+	2. The user was asked to guess
+
+	3. match the user-guessing number to the generated number
+"""
+def ex_06():
+    return 0
+
+
+
+if __name__ == '__main__':
+    #ex_01()
+    #ex_02()
+    # ex_03()
+    # ex_04()
+     ex_05()
+    # ex_06()

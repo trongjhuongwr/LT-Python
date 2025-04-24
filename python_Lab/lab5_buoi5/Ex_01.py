01a694093fe795164e65d021025ce22dbad3bae9
trongjhuongwr <nguyentronghuong05@gmail.com>
2025-04-24 15:11:44 +0700

    okeeee

--- python_Lab/lab5_buoi5/Ex_01.py
+++ python_Lab/lab5_buoi5/Ex_01.py
@@ -5,14 +5,17 @@ def ex1(list):
     print(sum_list)
 
 def ex2(list):
-    pass
+    result = 1
+    for item in list:
+        result *= item
+    print(result)
 
 def ex3(list):
     pass
 
 
 if __name__ == '__main__':
-    string_list = ['abc', 'xyz', 'aba', '1221']
+    #string_list = ['abc', 'xyz', 'aba', '1221']
     integer_list = []
     for i in range(10):
         a = random.randint(1, 100)
@@ -21,5 +24,5 @@ if __name__ == '__main__':
 
 
     ex1(integer_list)
-    # ex2(integer_list)
+    ex2(integer_list)
     #pass
